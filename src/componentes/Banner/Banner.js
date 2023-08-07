56b8835dbaf62b8e411806f58e280cd1651d5c46
Alany Cruz <115309188+lanybcr@users.noreply.github.com>
2023-08-07 11:32:23 -0300

    This is a new commit for what I originally planned to be amended

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+import "./Banner.css";
+
+export const Banner = () => {
+  // JSX
+  // fragment pode usar tb assim => <>/</>
+  return (
+    <Fragment>
+      <header className="banner">
+        <img
+          src="/imagens/banner.png"
+          alt="O banner principal da página do Organo"
+        />
+      </header>
+    </Fragment>
+  );
+};
---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,29 @@
+//import { useState } from 'react'
+import './CampoTexto.css'
+
+const CampoTexto = (props) => {
+
+    const placeholderModificada = `${props.placeholder}...` 
+
+    //let valor = ''
+
+    //const [valor, setValor]= useState('')
+
+    // setValor(evento.target.value)
+    //console.log(valor)
+
+    const aoDigitado = (evento) => {
+       props.aoAlterado(evento.target.value)
+    }
+
+    return (
+        <div className="campo-texto">
+            <label>
+                {props.label}
+            </label>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
+        </div>
+    )
+}
+
+export default CampoTexto
